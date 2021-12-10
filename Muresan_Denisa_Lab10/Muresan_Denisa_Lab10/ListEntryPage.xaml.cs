5482efc7a8a9f74334b441c3fdad6716fd8c4905
Denisa Muresan <Muresan.VS.Denisa@student.utcluj.ro>
2021-12-10 21:12:47 +0100

    Add project files.

---
+++ Muresan_Denisa_Lab10/Muresan_Denisa_Lab10/ListEntryPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Muresan_Denisa_Lab10.Models;
+
+namespace Muresan_Denisa_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListEntryPage : ContentPage
+    {
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetShopListsAsync();
+        }
+        async void OnShopListAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = new ShopList()
+            });
+        }
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ListPage
+                {
+                    BindingContext = e.SelectedItem as ShopList
+                });
+            }
+        }
+        public ListEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Muresan_Denisa_Lab10/Muresan_Denisa_Lab10/ListPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Muresan_Denisa_Lab10.Models;
+
+namespace Muresan_Denisa_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
